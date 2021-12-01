2cbb543649a10d110d595bd52d171d0ec486dbe4
Vanessa-Motta <mottavns@gmail.com>
2021-12-01 17:09:46 -0300

    fix:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
 
-export default function App(){
-  const [filmes, setFilmes]=useState([])
- 
-  useEffect(()=>{
-      axios.get('https://api.tvmaze.com/search/shows?q=star%20wras')
-        .then(res=>{
-          const dadosFilmes=res.data
-          setFilmes(dadosFilmes)
-      })
-  })
-  return(
+export default function App() {
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(() => { axios.get('https://api.tvmaze.com/search/shows?q=star%20wras').then((res) => {setFilmes(res.data)})})
+  return (
     <div>
       <h1>Listar os filmes</h1>
-      {filmes.map(filme => (
-      <li>
-        <strong>Título: </strong>
-        {filme.show.name}
-        <p>{filme.show.url}</p>
-      </li>
+      {filmes.map((filme) => (
+        <li>
+          <strong>Título: </strong>
+          {filme.show.name}
+          <p>{filme.show.url}</p>
+        </li>
       ))}
     </div>
   );
